686df1fe7916357352ce859bf14913336cc5513d
Gardenditrabia <32524343+Gardenditrabia@users.noreply.github.com>
2017-10-05 14:17:17 +0200

    Update cities.js
    - Aggiunta Dollet
    - Aggiunta Timber

--- geoJSON/cities.js
+++ geoJSON/cities.js
@@ -40,6 +40,32 @@ var cities = {
         type: 'Point', 
         coordinates: [-32.607421875, 71.58053179556501]
       }
+    }, {
+      type: 'Feature',
+      properties: {
+        featureType: "city",
+        name: "Dollet",
+        image: "img/cities/balbamb_garden.png",
+        icon: "img/icons/city2.png",
+        description: "Capitale del fu Impero di Dollet che in antichità si estendeva su tutto il continente di Galbadia. Attualmente è il centro del potere della Strega Astera. Tutte le comunicazioni in entrata ed in uscita dalla città sono bloccate da misteriosi fenomeni magici che interferiscono anche sulle immagini satellitari.  "
+      },
+      geometry: {
+        type: 'Point', 
+        coordinates: [-55.283203125, 75.6504309974655]
+      }
+    },{
+      type: 'Feature',
+      properties: {
+        featureType: "city",
+        name: "Timber",
+        image: "img/cities/balbamb_garden.png",
+        icon: "img/icons/city2.png",
+        description:` <a href="http://trabia.forumfree.it/?t=59368965#entry483386747" target="_blank">Clicca per accedere alla pagina sul sito</a> Timber è un polo ferroviario di rilevanza fondamentale - dalla sua stazione partono treni che raggiungono tutte le principali città mondiali. La sua importanza ha attratto in passato le brame di conquista di più di una Strega.`
+      },
+      geometry: {
+        type: 'Point', 
+        coordinates: : [-61.611328125, 62.63376960786814]
+      }
     },
   ]
 };
