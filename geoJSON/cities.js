6fe512029d2773d7b16d4ed6561da594fbddacdf
Gardenditrabia <32524343+Gardenditrabia@users.noreply.github.com>
2017-10-05 15:16:29 +0200

    Update cities.js
    Aggiunte
    - Balamb Town
    - Garden di Balamb
    - Trabia Town
    - Garden di Trabia

--- geoJSON/cities.js
+++ geoJSON/cities.js
@@ -6,26 +6,13 @@ var cities = {
       properties: {
         featureType: "city",
         name: "Garden di Balamb",
-        image: "img/cities/balbamb_garden.png",
+        image: "",
         icon: "img/icons/city2.png",
-        description: "Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare "
+        description: " Fondato da Cid Kramer, è il primo Garden ad essere stato istituito con lo scopo di sconfiggere e salvare la Strega Edea. In seguito è diventato la fonte di ispirazione che hanno portato alla creazione dei Garden di Galbadia e Trabia. E' il Garden da cui provengono gli Eroi SeeD e quello con la maggior rateo di SeeD d'élite. Dopo la sconfitta ottenuta nel tentativo di eradicare la Strega Astera da Dollet, il Garden di Balamb ha chiuso le iscrizioni al fine di migliorare l'addestramento dei SeeD già presenti diventando a tutti gli effetti un'accademia ancora più elitaria."
       },
       geometry: {
         type: 'Point', 
-        coordinates: [-10.986328125, 77.91566898632584]
-      }
-    },{
-      type: 'Feature',
-      properties: {
-        featureType: "city",
-        name: "Balamb city",
-        image: "img/cities/balbamb_garden.png",
-        icon: "img/icons/city2.png",
-        description: "Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare Scuola militare "
-      },
-      geometry: {
-        type: 'Point', 
-        coordinates: [2.109375, 77.65534600967776]
+        coordinates: [-18.80859375, 72.18180355624855]
       }
     },{
       type: 'Feature',
@@ -34,7 +21,7 @@ var cities = {
         name: "Balamb Town",
         image: "",
         icon: "img/icons/city2.png",
-        description: `<a href="http://trabia.forumfree.it/?t=59368965#entry483359142" target="_blank">Clicca per accedere alla pagina sul sito</a>`
+        description: `<a href="http://trabia.forumfree.it/?t=59368965#entry483359142" target="_blank">Clicca per accedere alla pagina sul sito</a> Città marittima di grande importanza anche e sopratutto per la vicinanza con il prestigioso Garden di Balamb e, per questo, da molti considerata la città più sicura al mondo.`
       },
       geometry: {
         type: 'Point', 
@@ -66,6 +53,34 @@ var cities = {
         type: 'Point', 
         coordinates: [-61.611328125, 62.63376960786814]
       }
+    },{
+      type: 'Feature',
+      properties: {
+        featureType: "city",
+        name: "Trabia Town",
+        image: "",
+        icon: "img/icons/city2.png",
+        description:` <a href="http://trabia.forumfree.it/?t=59368965#entry483188314" target="_blank">Clicca per accedere alla pagina sul sito</a> La fondazione della città, che risale a pochi anni dopo la fine della guerra della Strega Artemisia, ha causato la sparizione e l'incorporazione di numerosi piccoli centri abitati del continente di Trabia. Attualmente la città è un forte polo economico con molte possibilità di lavoro ed in cui la ricchezza è equamente distribuita tra la popolazione.`
+      },
+      geometry: {
+        type: 'Point', 
+        coordinates: [-17.9296875, 80.23850054635392]
+      }
+    },{
+      type: 'Feature',
+      properties: {
+        featureType: "city",
+        name: "Garden di Trabia",
+        image: "",
+        icon: "img/icons/city2.png",
+        description:` Dopo la sconfitta della Strega Artemisia è stata messa in moto una macchina economica per ricostruire il Garden di Trabia precedentemente distrutto dal famoso attacco missilistico da parte di Galbadia ai suoi danni. Il progetto - nato da Balamb e promosso dalla stessa Galbadia - ha messo nelle mani di Hector Mayer la nuova accademia SeeD. Alla sua morte, i figli Heimmerich ed Emily hanno preso le redini del Garden e portando avanti la guerra dei suoi SeeD - i Liberi Fatali - contro la Strega Astera. `
+      },
+      geometry: {
+        type: 'Point', 
+        coordinates: [-17.9296875, 80.23850054635392]
+      }
     },
+
   ]
 };
+
